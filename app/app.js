0f3bd639dfdb6b3615320b92a35f3f66d66d1d1e
David Dinh <ddinh1@uci.edu>
2017-10-15 22:34:20 -0700

    Install CORS module.

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -29,6 +30,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, '../client/public')));
+
+app.use(cors());
 
 app.use('/', routes);
 
